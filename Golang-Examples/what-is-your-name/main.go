30d17609d49437a859e9e22cc0830caab891ced0
Sammi Turner <ms.sammi.turner@gmail.com>
2024-03-28 18:22:58 +0000

    Update main.go

--- Golang-Examples/what-is-your-name/main.go
+++ Golang-Examples/what-is-your-name/main.go
@@ -7,22 +7,16 @@ import (
 	"strings"
 )
 
-// Flushes stdout.
-func FlushStdout() {
-	writer := bufio.NewWriter(os.Stdout)
-	writer.Flush()
-}
-
 // Prompts the user with a given string and returns their input.
 func UserInput(s string) string {
 	fmt.Print(s)
-	FlushStdout()
 	reader := bufio.NewReader(os.Stdin)
 	userInput, _ := reader.ReadString('\n')
 	return strings.TrimSuffix(userInput, "\n")
 }
 
 func main() {
-	name := UserInput("What is your name? ")
-	fmt.Printf("Hello %s!\n", name)
+	first := UserInput("What is first your name? ")
+	last := UserInput("What is your last name? ")
+	fmt.Printf("Hello %s %s!\n", first, last)
 }
