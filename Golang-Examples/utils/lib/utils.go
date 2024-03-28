6a801c73892b24730e5ec631da32b8f9a8135618
Sammi Turner <ms.sammi.turner@gmail.com>
2024-03-28 18:28:55 +0000

    Update utils.go

--- Golang-Examples/utils/lib/utils.go
+++ Golang-Examples/utils/lib/utils.go
@@ -40,7 +40,6 @@ func FlushStdout() {
 // Prompts the user with a given string and returns their input.
 func UserInput(s string) string {
 	fmt.Print(s)
-	FlushStdout()
 	reader := bufio.NewReader(os.Stdin)
 	userInput, _ := reader.ReadString('\n')
 	return strings.TrimSuffix(userInput, "\n")
