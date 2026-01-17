6110a0a970ac873b1d41f6ef31d80b71a1d78031
Sammi Turner <ms.sammi.turner@gmail.com>
2026-01-17 12:59:27 +0000

    Update main.go

--- Golang-Examples/cli-arg-demo/main.go
+++ Golang-Examples/cli-arg-demo/main.go
@@ -1,28 +1,18 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 )
 
 func main() {
-	if err := run(); err != nil {
-		fmt.Fprintln(os.Stderr, "Error:", err)
-		os.Exit(1)
-	}
-}
-
-func run() error {
-	args := os.Args[1:]
-
+	args := os.Args
 	switch len(args) {
-	case 0:
-		return errors.New("No argument supplied")
 	case 1:
-		fmt.Println(args[0])
-		return nil
+		fmt.Println("No argument supplied.")
+	case 2:
+		fmt.Println(args[1])
 	default:
-		return errors.New("Only one argument is allowed")
+		fmt.Println("Only one argument is allowed.")
 	}
 }
