0b9f67c30fae80fc5cb89fd867d5cab0aeb21ac5
Sammi Turner <ms.sammi.turner@gmail.com>
2026-01-20 01:13:22 +0000

    Add files via upload

--- Golang-Examples/utils/main.go
+++ Golang-Examples/utils/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	utils "utils/lib"
 )
 
 func main() {
-	test := "Hello, 世界✨"
-    size := utils.RuneLength(test)
-	for i := 0; i < size; i++ {
-		r, _ := utils.NthRune(test, i)
-		fmt.Printf("%c\n", r)
-	}
+    r, err := ReadFile("test.txt")
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
+    fmt.Printf("%s\n", r)
 }
